32aaa708c1a1c3a3934387c74a795a09255e7d50
Frank Pizzella <fpizz210@gmail.com>
2023-03-31 00:57:05 -0400

    Retrieved Data from Server and Displayed with HTML

---
+++ CambridgeChallenge/CambridgeChallenge/Models/EventModel.cs
@@ -0,0 +1,11 @@
+﻿namespace CambridgeChallenge.Models
+{
+    public class EventModel
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Start { get; set; }
+        public string End { get; set; }
+        public string Link { get; set; }
+    }
+}
--- CambridgeChallenge/CambridgeChallenge/Pages/Index.cshtml.cs
+++ CambridgeChallenge/CambridgeChallenge/Pages/Index.cshtml.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CambridgeChallenge.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+using Octokit;
+using System.Diagnostics;
 
 namespace CambridgeChallenge.Pages
 {
@@ -7,14 +11,42 @@ namespace CambridgeChallenge.Pages
     {
         private readonly ILogger<IndexModel> _logger;
 
+        private HttpClient _httpClient;
+
+        [BindProperty]
+        public List<EventModel> events { get; set; }
+
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
+            _httpClient = new HttpClient();
         }
 
-        public void OnGet()
+        public async Task OnGetAsync()
         {
+            await getEventsData();
+        }
 
+        public async Task getEventsData()
+        {
+            string path = "https://www.cambridgema.gov/GetEvents.ashx?format=json&view=homepage";
+
+            HttpRequestMessage httpRequestMessage = new HttpRequestMessage();
+
+            try
+            {
+                HttpResponseMessage response = await _httpClient.GetAsync(path);
+
+                var responseContent = await response.Content.ReadAsStringAsync();
+
+                this.events = JsonConvert.DeserializeObject<List<EventModel>>(responseContent);
+
+                Debug.WriteLine("Events: " + this.events[0].Title);
+            }
+            catch (HttpRequestException exception) 
+            {
+                Debug.WriteLine("An HTTP request exception occurred.", exception.Message);
+            }
         }
     }
 }
